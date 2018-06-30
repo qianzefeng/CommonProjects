b2c3d25fbf77cabcb58c93caaefcd84357ba1446
fengliguang <260202456@qq.com>
2018-06-30 20:18:53 +0800

    《C++ API设计》书籍示例代码

---
+++ ApiBook/03Patterns_Facade/main.cpp
@@ -0,0 +1,23 @@
+/// -*- tab-width: 4; c-basic-offset: 4; indent-tabs-mode: t -*-
+///
+/// \file   main.cpp
+/// \author Martin Reddy
+/// \brief  An example of the Facade design pattern.
+///
+/// Copyright (c) 2010, Martin Reddy. All rights reserved.
+/// Distributed under the X11/MIT License. See LICENSE.txt.
+/// See http://APIBook.com/ for the latest version.
+///
+
+#include "facade.h"
+#include <iostream>
+
+int main(int, char **)
+{
+	// create the facade object and call one of its functions
+	Facade facade;
+	facade.DoSomething();
+
+	system("pause");
+	return 0;
+}
---
+++ ApiBook/VS_Clear.bat
@@ -0,0 +1,34 @@
+@echo off
+setlocal enabledelayedexpansion  
+
+for /r . %%a in (Debug) do (  
+  if exist %%a (
+  echo "delete" %%a
+  rd /s /q "%%a" 
+ )
+)
+
+for /r . %%a in (Release) do (  
+  if exist %%a (
+  echo "delete" %%a
+  rd /s /q "%%a" 
+ )
+)
+
+for /r . %%a in (ipch) do (  
+  if exist %%a (
+  echo "delete" %%a
+  rd /s /q "%%a" 
+ )
+)
+
+for /r . %%a in (*.sdf) do (  
+  if exist %%a (
+  echo "delete" %%a
+  del "%%a" 
+ )
+)
+
+del /F /S /Q *.aps *.idb *.ncp *.obj *.pch *.sbr *.tmp *.pdb *.bsc *.ilk *.res *.opt *.ncb *.suo *.manifest *.dep *.user
+
+pause
